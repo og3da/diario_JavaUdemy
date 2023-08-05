ccdb94afd82b46e2c7d5a77e65f8c4a2bfe9c175
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-08-05 14:15:22 -0300

    exercicio resolvido com POO

--- secao8_POO/exercicioComOO/src/application/Program.java
+++ secao8_POO/exercicioComOO/src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         Triangle x, y;
-        x = new Triangle();
+        x = new Triangle(); // instanciando var, ela se torna um objeto
         y = new Triangle();
 
         System.out.println("Enter the measures of triangle X: ");
@@ -22,12 +22,8 @@ public class Program {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-
+        double areaX = x.area();
+        double areaY = y.area();
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
         if (areaX > areaY) {
