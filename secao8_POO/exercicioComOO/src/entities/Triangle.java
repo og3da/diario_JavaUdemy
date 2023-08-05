296f514fa5491b472e84c40674df9eb90203de1d
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-08-05 13:33:13 -0300

    programação orientada a objeto
    resolvendo um exercicio sem POO e com POO para mostrar os beneficios de Classes

---
+++ secao8_POO/exercicioComOO/src/entities/Triangle.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Triangle {
+
+    // declarando atributos da classe triangulo
+    // (lados do triangulo)
+    public double a;
+    public double b;
+    public double c;
+}
