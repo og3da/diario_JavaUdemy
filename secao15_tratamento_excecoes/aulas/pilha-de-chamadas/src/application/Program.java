af7393c261e24f098e5df70f2871ca64ddb81d91
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-10-24 20:37:17 -0300

    Add files via upload

---
+++ secao15_tratamento_excecoes/aulas/pilha-de-chamadas/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+
+		method1();
+
+		System.out.println("End of program");
+
+	}
+
+	public static void method1() {
+		System.out.println("*** METHOD1 START ***");
+		method2();
+		System.out.println("*** METHOD1 END ***");
+	}
+
+	public static void method2() {
+		System.out.println("*** METHOD2 START ***");
+		Scanner sc = new Scanner(System.in);
+		
+		String[] vect = sc.nextLine().split(" ");
+		int position = sc.nextInt();
+		System.out.println(vect[position]);
+
+		sc.close();
+		System.out.println("*** METHOD2 END ***");
+	}
+}
