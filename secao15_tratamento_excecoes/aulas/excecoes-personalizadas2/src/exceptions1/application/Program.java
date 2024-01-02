6bbfc634bbb8e849fa8c32d8afcdfba7a2e85b6d
og3da <rapha488@gmail.com>
2024-01-02 18:22:11 -0300

    aula excecoes personalizadas

---
+++ secao15_tratamento_excecoes/aulas/excecoes-personalizadas2/src/exceptions1/application/Program.java
@@ -0,0 +1,4 @@
+package exceptions1.application;
+
+public class Program {
+}
---
+++ secao15_tratamento_excecoes/aulas/excecoes-personalizadas2/src/exceptions1/model/Reservation.java
@@ -0,0 +1,40 @@
+package exceptions1.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public long duration() {
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkin, Date checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "Room " + roomNumber + ", " +
+                "check-in: " + sdf.format(checkin) + ", " +
+                "check-out: " + sdf.format(checkout) + ", " +
+                duration() + " nights";
+    }
+}
