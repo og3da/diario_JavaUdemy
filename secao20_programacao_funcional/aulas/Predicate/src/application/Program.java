c674401e088923199567ac3a333d0643c6161053
og3da <rapha488@gmail.com>
2024-01-04 15:28:58 -0300

    Predicate: usando expressão lambda declarada

--- secao20_programacao_funcional/aulas/Predicate/src/application/Program.java
+++ secao20_programacao_funcional/aulas/Predicate/src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import entities.Product;
-import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,7 +15,9 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate); // usando a referência ao metodo
+        // usando expressão lambda declarada
+        Predicate<Product> pred = p -> p.getPrice() >= 100;
+        list.removeIf(pred);
 
         for (Product p : list) {
             System.out.println(p);
