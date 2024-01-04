64e5ec2b90cedde1d943426c048254b850e1c236
og3da <rapha488@gmail.com>
2024-01-04 16:47:26 -0300

    Function: implementando interface

---
+++ secao20_programacao_funcional/aulas/Function/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import entities.Product;
+import util.NameUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        /* nesse caso p/alterar cada item da lista precisamos converter p/stream
+           stream é um tipo de dado que armazena valores
+
+           a função map é usada para alterar cada valor armazenado em uma stream,
+           gerando uma nova stream com os valores alterados
+
+           collect(Collectors.toList()): converte em lista uma stream
+         */
+        List<String> names = list.stream().map(new NameUpdate()).collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+    }
+}
