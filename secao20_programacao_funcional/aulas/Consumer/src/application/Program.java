a875c1d9303367eb979d350467692793c9f0b44a
og3da <rapha488@gmail.com>
2024-01-04 15:52:12 -0300

    Consumer: usando expressao lambda in-line

--- secao20_programacao_funcional/aulas/Consumer/src/application/Program.java
+++ secao20_programacao_funcional/aulas/Consumer/src/application/Program.java
@@ -15,9 +15,8 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        // usando expressao lambda declarada
-        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
-        list.forEach(cons);
+        // usando expressao lambda in-line
+        list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 
         list.forEach(System.out::println);
     }
