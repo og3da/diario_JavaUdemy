3e03fced333c8c87d3f90c33cd605151e4c5dc01
og3da <rapha488@gmail.com>
2024-01-09 13:20:02 -0300

    deletando dados no banco com JDBC

--- secao21_acesso_database_JDBC/aulas/projetoJDBC/src/application/Program.java
+++ secao21_acesso_database_JDBC/aulas/projetoJDBC/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -74,7 +75,7 @@ public class Program {
         }*/
 
         // ATUALIZANDO DADOS
-        PreparedStatement st = null;
+        /*PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
@@ -94,6 +95,30 @@ public class Program {
             System.out.println("done, rows affected: " + rowsAffected);
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }*/
+
+        // DELETANDO DADOS
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            // comando SQL
+            st = conn.prepareStatement(
+                    "DELETE FROM department "
+                            + "WHERE "
+                            + "Id = ?"
+            );
+
+            st.setInt(1, 2);
+
+            int rowsAffected = st.executeUpdate();
+            System.out.println("done, rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
