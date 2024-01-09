96878eaa5ed884c4d2ca2244eaef9b00f4bc96e9
og3da <rapha488@gmail.com>
2024-01-09 10:30:47 -0300

    consulta no banco de dados usando JDBC

--- secao21_acesso_database_JDBC/aulas/projetoJDBC/src/db/DB.java
+++ secao21_acesso_database_JDBC/aulas/projetoJDBC/src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -36,13 +34,34 @@ public class DB {
         }
     }
 
-        private static Properties loadProperties() {
+    private static Properties loadProperties() {
         try (FileInputStream fs = new FileInputStream("C:\\Users\\rapha\\Documents\\Workspace-IntelliJ\\diario_JavaUdemy\\secao21_acesso_database_JDBC\\aulas\\projetoJDBC\\db.properties")) {
             Properties props = new Properties();
             props.load(fs);
             return props;
         } catch (IOException e) {
             throw new DbException(e.getMessage());
+        }
+    }
+
+    // metodos auxiliares para fechar Statement & ResultSet
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
         }
     }
 }
